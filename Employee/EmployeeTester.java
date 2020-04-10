4f2630c3607b0de1e8700bf72b55de11a7906839
dpipi <s3805554@student.rmit.edu.au>
2020-04-10 12:45:18 +1000

    upload week 7 folder and Employee class

---
+++ Employee/EmployeeTester.java
@@ -0,0 +1,23 @@
+package Employee;
+
+public class EmployeeTester
+{
+    public static void main(String[] args)
+    {
+        /* Task 3.1.2 - Interacting with objects */
+
+        // Task A - Create two new Employees Jill and John
+        Employee e1 = new Employee("Jill", "Sales", "Manager");
+        Employee e2 = new Employee("John", "Legal", "Clerk");
+
+        // Task B - display team and position for jill
+        //System.out.println("Jills team:" + e1.getTeam());
+        //System.out.println("Jills position:" + e1.getPosition());
+
+        // Task C - display team and position for john
+        //System.out.println("Jills team:" + e2.getTeam());
+        //System.out.println("Jills position:" + e2.getPosition());
+        e1.printDetails();
+        e2.printDetails();
+    }
+}
